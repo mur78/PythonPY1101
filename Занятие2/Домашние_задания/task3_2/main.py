90d71e863be0213d9560baeee64a74c9a84f873b
Mur78 <drivem78@mail.ru>
2021-11-28 23:32:50 +0300

    28.11 ok

--- Занятие2/Домашние_задания/task3_2/main.py
+++ Занятие2/Домашние_задания/task3_2/main.py
@@ -6,9 +6,9 @@ def positive_check_int(fn):
         # z = map(positive_check_int, *args)
 
         for args in *args:
-            if isinstance(args, int):
+            if isinstance(*args, int):
                 result = fn(*args)
-            elif not isinstance(args, int):
+            elif not isinstance(*args, int):
                 raise TypeError(f"Результатом выполнения функции {fn} должен быть числом")
 
         return result
--- Занятие2/Домашние_задания/task3_3/main.py
+++ Занятие2/Домашние_задания/task3_3/main.py
@@ -1,23 +1,41 @@
-def output_type_int(fn):
+def output_type_l(fn):
     def wrapper(*args, **kwargs):
-
+        try:
+        next(*args)
+        next(**kwargs)
         # через try exept (итератор проверяем next) - проверяем до результата - используем кортеж и enumerate
+            for index, arg in enumerate(args):
+                print(f"Позиционный аргумент {index}: {arg}")
 
-        result = fn(*args, **kwargs)
-        if not isinstance(result, int):
-            raise TypeError("Объект <название или номер позиционного аргумента> <значение аргумента> не является итерируемым")
+            for key, kwarg in kwargs.items():
+                print(f"Именованный аргумент {key}: {kwarg}")
+
+                result = fn(*args, **kwargs)
+
+            except StopIteration:
+            print("Итератор закончился")
+            print_exc()
+
+# if not isinstance(result, int):
+            #     raise TypeError("Объект <название или номер позиционного аргумента> <значение аргумента> не является итерируемым")
 
 
     return wrapper
 
 
-@output_type_int
-def return_int() -> int:
+@output_type_l
+def return_list() -> list:
+    pass
+
+@output_type_l
+def return_dict() -> dict:
     pass
 
 
 
 
+
 if __name__ == "__main__":
-    return_int(1, 3)
-    return_int("1", "7")
+    return_list(1, 3)
+    return_dict(t1=5, z1=7)
+
